910ef92a200c9e42a28d6605f07da6a7623fd4ec
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-06-15 00:10:30 +0700

    init.el: Remove package-initialize

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,12 +6,9 @@
 
 ;;; Code:
 
+;; GET OF MY LAWN REEEEEEE
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;(package-initialize)
 
 (require 'package)
 
