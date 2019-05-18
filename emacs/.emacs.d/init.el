a86ec55108281bd3dba1826989131004484fb53b
Muhammad Kaisar Arkhan <kaisar@arkhan.io>
2019-05-18 17:02:33 +0200

    Add html to web-mode list

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -256,12 +256,12 @@
 ;; HTML
 (use-package web-mode
   :mode (("\\.phtml\\'" . web-mode)
-  	 ("\\.tpl\\.php\\'" . web-mode)
-	 ("\\.[agj]sp\\'" . web-mode)
-	 ("\\.as[cp]x\\'" . web-mode)
-	 ("\\.erb\\'" . web-mode)
-	 ("\\.mustache\\'" . web-mode)
-	 ("\\.djhtml\\'" . web-mode)
+         ("\\.tpl\\.php\\'" . web-mode)
+         ("\\.[agj]sp\\'" . web-mode)
+         ("\\.as[cp]x\\'" . web-mode)
+         ("\\.erb\\'" . web-mode)
+         ("\\.mustache\\'" . web-mode)
+         ("\\.djhtml\\'" . web-mode)
          ("\\.html?\\'" . web-mode)))
 
 ;; Markdown
