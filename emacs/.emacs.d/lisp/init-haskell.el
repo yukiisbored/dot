ca4813a5106fab4dcfd47512ce1e0936f541f848
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-12-04 10:49:42 +0100

    Integrate haskell with LSP

--- emacs/.emacs.d/lisp/init-haskell.el
+++ emacs/.emacs.d/lisp/init-haskell.el
@@ -1,5 +1,12 @@
 ;;; init-haskell.el --- Setup environment for haskell
 
-(use-package haskell-mode)
+(use-package haskell-mode
+  :hook
+  (haskell-mode . haskell-interactive-mode))
+
+(use-package lsp-haskell
+  :hook
+  (haskell-mode . lsp)
+  (haskell-literate-mode . lsp))
 
 (provide 'init-haskell)
