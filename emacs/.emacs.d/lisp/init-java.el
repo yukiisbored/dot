9b81c6a7b41586583b8a560651b8099f534c7703
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-12-30 02:07:17 +0100

    Enable lombok for java if exists

--- emacs/.emacs.d/lisp/init-java.el
+++ emacs/.emacs.d/lisp/init-java.el
@@ -2,6 +2,11 @@
 
 (use-package lsp-java
   :hook
-  (java-mode . lsp))
+  (java-mode . lsp)
+  :init
+  (setq lombok-jar-path (expand-file-name "lombok.jar" user-emacs-directory))
+  :config
+  (when (file-exists-p lombok-jar-path)
+    (add-to-list 'lsp-java-vmargs (concat "-javaagent:" lombok-jar-path))))
 
 (provide 'init-java)
