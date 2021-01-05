8422a8f62ff2534130aaed8443ff25b6093985fb
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2021-01-05 17:42:32 +0100

    Drop racer and flycheck-rust in favor of lsp-mode

--- emacs/.emacs.d/lisp/init-rust.el
+++ emacs/.emacs.d/lisp/init-rust.el
@@ -1,22 +1,12 @@
 ;;; init-rust.el --- Setup rust mode
 
 (use-package rust-mode
-  :mode "\\.rs\\'")
-
-(use-package flycheck-rust
-  :init
-  (with-eval-after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)))
-
-(use-package racer
-  :init
-  (add-hook 'rust-mode-hook 'racer-mode)
-  (add-hook 'racer-mode-hook 'eldoc-mode)
-  :config
-  (define-key rust-mode-map (kbd "TAB") 'company-indent-or-complete-common))
+  :mode "\\.rs\\'"
+  :hook
+  (rust-mode . lsp))
 
 (use-package cargo
-  :init
-  (add-hook 'rust-mode-hook 'cargo-minor-mode))
+  :hook
+  (rust-mode . cargo-minor-mode))
 
 (provide 'init-rust)
