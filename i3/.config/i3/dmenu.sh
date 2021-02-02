9047a980cb0880638cdd4816a8fbac9d50657ca8
User Yuki <yuki@onoe.yukiisbo.red>
2021-02-02 23:52:39 +0100

    Update i3 setup

--- i3/.config/i3/dmenu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec dmenu_run -nf '#5F5F5F' -nb '#FDFDE7' -sb '#FFAFAF' -sf '#D40000' -fn 'IBM Plex Mono-10' -p 'exec '
--- i3/.config/i3/i3lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec i3lock --image ~/.config/i3/wallpaper.png --tiling --color fdfde7
