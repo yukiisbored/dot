e172d3f75d050f3c48bf50808e6e70c65c83bae7
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2021-04-10 18:32:49 +0200

    Hack to get direnv working before lsp

--- emacs/.emacs.d/lisp/init-lsp.el
+++ emacs/.emacs.d/lisp/init-lsp.el
@@ -1,12 +1,14 @@
 ;;; init-lsp.el --- Language server support for Emacs.
 
 (use-package lsp-mode
+  :after direnv
   :hook
   (lsp-mode . lsp-enable-which-key-integration)
   (lsp-mode . lsp-lens-mode)
   :init
   (setq lsp-enable-file-watchers nil
-        lsp-keymap-prefix "C-c l"))
+        lsp-keymap-prefix "C-c l")
+  (advice-add 'lsp :before 'direnv-update-environment)) ;; Hack to get direnv running before lsp
 
 (use-package lsp-ui
   :after lsp-mode)
