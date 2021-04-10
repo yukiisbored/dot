8932f8e9c082f64f4f8c30d964b416f1fb531803
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2021-04-10 12:44:32 +0200

    Add pipenv support because I hate myself

--- emacs/.emacs.d/lisp/init-python.el
+++ emacs/.emacs.d/lisp/init-python.el
@@ -20,6 +20,13 @@
 (use-package poetry)
 (use-package live-py-mode)
 
+(use-package pyvenv)
+(use-package pipenv
+  :hook (python-mode . pipenv-mode)
+  :init
+  (setq pipenv-projectile-after-switch-function
+        'pipenv-projectile-after-switch-extended))
+
 (use-package lsp-python-ms
   :if (or (string-equal system-type "gnu/linux")
           (string-equal system-type "windows-nt")
