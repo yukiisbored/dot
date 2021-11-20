5248f7f89c9a6716b40e64e6a9d4dac6ca6ee69c
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-11-21 00:12:16 +0100

    Remove scala

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -535,23 +535,6 @@
 (use-package nix-mode
   :mode "\\.nix\\'")
 
-(use-package scala-mode
-  :interpreter (("scala" . scala-mode)))
-
-(use-package sbt-mode
-  :init
-  (setq sbt:program-options '("-Dsbt.supershell=false"))
-  :config
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map))
-
-(use-package lsp-metals
-  :hook
-  (scala-mode . lsp))
-
-
 (use-package clojure-mode)
 (use-package cider)
 
