743f0d6538442e133d1fa544207acaed076aae4e
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-11-21 00:13:08 +0100

    Remove mu4e and irc setup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -805,20 +805,3 @@
         org-roam-ui-open-on-start t))
 
 (use-package htmlize)
-
-(if (file-directory-p "/usr/local/share/emacs/site-lisp/mu4e")
-    (setq mu4e-directory "/usr/local/share/emacs/site-lisp/mu4e")
-  (setq mu4e-directory "/usr/share/emacs/site-lisp/mu4e"))
-
-(setq email-setup-el (expand-file-name "email-setup.el" user-emacs-directory))
-
-(when (file-directory-p mu4e-directory)
-  (add-to-list 'load-path mu4e-directory)
-  (require 'mu4e)
-  (when (file-exists-p email-setup-el)
-    (load-file email-setup-el)))
-
-(setq irc-setup-el (expand-file-name "irc-setup.el" user-emacs-directory))
-
-(when (file-exists-p irc-setup-el)
-  (load-file irc-setup-el))
