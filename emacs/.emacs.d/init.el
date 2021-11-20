d2a52723d206e104cfa5fb58361af2405f114078
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-11-21 00:11:56 +0100

    Remove treemacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -366,25 +366,6 @@
 (use-package projectile-git-autofetch
   :after projectile)
 
-;; Treemacs
-(use-package treemacs
-  :bind
-  (("M-0"       . treemacs-select-window)
-   ("C-x t 1"   . treemacs-delete-other-windows)
-   ("C-x t t"   . treemacs)
-   ("C-x t B"   . treemacs-bookmark)
-   ("C-x t C-t" . treemacs-find-file)
-   ("C-x t M-t" . treemacs-find-tag))
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window)))
-
-(use-package treemacs-projectile
-  :after (treemacs projectile))
-
-(use-package treemacs-magit
-  :after (treemacs magit))
-
 ;; Editorconfig
 (use-package editorconfig
   :hook ((after-init . editorconfig-mode)))
