663ed31f200231a34634987a2cbd5fcd7570da80
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2022-03-05 21:02:30 +0100

    Hack desktop files
    Due to Plasma's (stupid) requirement for desktop files to possess an
    executable bit, a hack is required to copy over the desktop files
    outside the Nix store and apply an executable bit on them.
    
    This sucks.

--- modules/generic.nix
+++ modules/generic.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   xdg = {
     enable = true;
@@ -7,4 +8,13 @@
   targets.genericLinux.enable = true;
 
   programs.bash.enable = true;
+
+  home.activation = {
+    hackApplications = lib.hm.dag.entryAfter ["linkGeneration"] ''
+      $DRY_RUN_CMD mkdir $VERBOSE_ARG -p $HOME/.local/share/applications/nix/
+      $DRY_RUN_CMD rm $VERBOSE_ARG -f $HOME/.local/share/applications/nix/*.desktop
+      $DRY_RUN_CMD cp $VERBOSE_ARG -L $HOME/.nix-profile/share/applications/*.desktop $HOME/.local/share/applications/nix/
+      $DRY_RUN_CMD chmod $VERBOSE_ARG -R u+w,a+x $HOME/.local/share/applications/nix/
+    '';
+  };
 }
