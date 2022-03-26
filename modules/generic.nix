d297a97e59853e6d93a287693d04a7dde10dca98
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2022-03-26 18:24:45 +0100

    Fix issue with hackApplications on bootstrap

--- modules/generic.nix
+++ modules/generic.nix
@@ -13,7 +13,7 @@
     hackApplications = lib.hm.dag.entryAfter ["linkGeneration"] ''
       $DRY_RUN_CMD mkdir $VERBOSE_ARG -p $HOME/.local/share/applications/nix/
       $DRY_RUN_CMD rm $VERBOSE_ARG -f $HOME/.local/share/applications/nix/*.desktop
-      $DRY_RUN_CMD cp $VERBOSE_ARG -L $HOME/.nix-profile/share/applications/*.desktop $HOME/.local/share/applications/nix/
+      $DRY_RUN_CMD [ -d $HOME/.nix-profile/share/applications ] && cp $VERBOSE_ARG -L $HOME/.nix-profile/share/applications/*.desktop $HOME/.local/share/applications/nix/
       $DRY_RUN_CMD chmod $VERBOSE_ARG -R u+w,a+x $HOME/.local/share/applications/nix/
     '';
   };
