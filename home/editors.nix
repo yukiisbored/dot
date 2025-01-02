604a97596031c159ebe59ceb626d6bf922e6209b
Yuki Langley <hi@yukiisbo.red>
2025-01-02 21:57:23 +0100

    Setup for lazyvim

--- home/editors.nix
+++ home/editors.nix
@@ -7,6 +7,22 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+
+    extraPackages = with pkgs; [
+      git
+      lazygit
+
+      findutils
+      fzf
+      ripgrep
+      fd
+
+      lua-language-server
+      stylua
+
+      nodejs_22
+
+    ];
   };
 
   programs.vscode.enable = true;
